821386adecbda5d7f4c2f7b5fb256370ae521467
Takuro Aoya <aoya.takuro@jp.fujitsu.com>
2016-01-27 00:14:10 +0900

    add Shitei GroupSelect

--- app/components/shitai/shitai.js
+++ app/components/shitai/shitai.js
@@ -9,11 +9,12 @@
   angular
     .module('imatomo.components.shitai', [
       'imatomo.service.shitaies',
-      'imatomo.service.profiles'
+      'imatomo.service.profiles',
+      'imatomo.service.groups'
     ])
     .controller('ShitaiController', ShitaiController);
 
-  ShitaiController.$inject = ['$location', 'ShitaiesService', 'ProfilesService'];
+  ShitaiController.$inject = ['$location', 'ShitaiesService', 'ProfilesService', 'GroupsService'];
 
   /**
    * ShitaiController
@@ -21,11 +22,13 @@
    * @class ShitaiController
    * @constructor
    */
-  function ShitaiController($location, ShitaiesService, ProfilesService) {
+  function ShitaiController($location, ShitaiesService, ProfilesService, GroupsService) {
     console.log('ShitaiController Constructor');
     this.$location = $location;
     this.ShitaiesService = ShitaiesService;
     this.ProfilesService = ProfilesService;
+    this.GroupsService = GroupsService;
+    this.profile = ProfilesService.getStorageProfile();
   }
 
   /**
@@ -37,6 +40,9 @@
   ShitaiController.prototype.activate = function() {
     console.log('ShitaiController activate Method');
     vm = this;
+
+    var groupList = vm.GroupsService.findGroups();
+    vm.groupList = groupList;
   };
 
   /**
@@ -61,11 +67,13 @@
       time: vm.time,
       comment : (vm.comment === undefined ? '' : vm.comment),
       place: (vm.place === undefined ? '' : vm.place),
+      group: (vm.place === undefined ? '' : vm.group),
       createtimestamp : Firebase.ServerValue.TIMESTAMP
     };
 
     // Firebaseに追加
     vm.ShitaiesService.addShitai(shitai, function() {
+      console.log('vm.group:' + vm.group);
       // shitailistへ遷移
       vm.$location.path('shitailist');
     });
@@ -86,6 +94,16 @@
     console.log('close');
     vm.status = '';
     vm.message = '';
+  };
+
+  ShitaiController.prototype.searchMember = function(group) {
+    console.log('ShitaiController searchMember Method');
+    for (var i = 0; i < group.members.length; i++) {
+      if (group.members[i].userid === vm.profile.userid) {
+        return true;
+      }
+    }
+    return false;
   };
 
 })();
